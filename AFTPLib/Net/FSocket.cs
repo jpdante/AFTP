e10c38bf8ec00525565a719410b34a1fc4f3f781
Joao Pedro <joaopdante@gmail.com>
2019-04-02 09:32:08 -0300

    Add socket and connection check

--- AFTPLib/Net/FSocket.cs
+++ AFTPLib/Net/FSocket.cs
@@ -7,8 +7,10 @@ namespace AFTPLib.Net
         private Socket _socket;
 
         public FSocket() {
-            
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
+        
+        public bool IsConnected => !((_socket.Poll(1000, SelectMode.SelectRead) && (_socket.Available == 0)) || !_socket.Connected);
 
     }
 }
