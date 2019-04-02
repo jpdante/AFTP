03e76007a96dbf7f26d6de490841442751c83d2c
Joao Pedro <joaopdante@gmail.com>
2019-04-02 10:31:49 -0300

    Update command ID

--- AFTPLib/Protocol/Commands/FinishHandshake.cs
+++ AFTPLib/Protocol/Commands/FinishHandshake.cs
@@ -3,6 +3,6 @@ using ProtoBuf;
 namespace AFTPLib.Protocol.Commands {
     [ProtoContract]
     public class FinishHandshake {
-        [ProtoMember(1)] public byte Command = 2;
+        [ProtoMember(1)] public byte Command = 1;
     }
 }
--- AFTPLib/Protocol/Commands/GetVersion.cs
+++ AFTPLib/Protocol/Commands/GetVersion.cs
@@ -3,7 +3,7 @@ using ProtoBuf;
 namespace AFTPLib.Protocol.Commands {
     [ProtoContract]
     public class GetVersion {
-        [ProtoMember(1)] public byte Command = 1;
+        [ProtoMember(1)] public byte Command = 2;
         [ProtoMember(2)] public string Version;
     }
 }
