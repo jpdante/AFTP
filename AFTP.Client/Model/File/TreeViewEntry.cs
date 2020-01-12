974ec7ab293fbb11f7c707b98308377d0acb7449
João Pedro <joaopdante@gmail.com>
2020-01-12 12:27:38 -0300

    Make TreeView work

---
+++ AFTP.Client/Model/File/TreeViewEntry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace AFTP.Client.Model.File {
+    public class TreeViewEntry : INotifyPropertyChanged  {
+
+        public string Path { get; set; }
+        public string Directory { get; set; }
+        public Bitmap Icon { get; set; }
+
+        private bool _isNodeExpanded;
+        private bool _isNodeSelected;
+
+        public bool IsNodeExpanded {
+            get => _isNodeExpanded;
+            set {
+                _isNodeExpanded = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool IsNodeSelected {
+            get => _isNodeSelected;
+            set {
+                _isNodeSelected = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<TreeViewEntry> Children { get; set; }
+
+        public TreeViewEntry(string name, string path) {
+            Directory = name;
+            Path = path;
+            _isNodeExpanded = true;
+            _isNodeSelected = false;
+            Icon = Properties.Resources.Folder;
+            Children = new ObservableCollection<TreeViewEntry>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
--- AFTP.Client/View/Explorer/HorizontalServerOnly.xaml.cs
+++ AFTP.Client/View/Explorer/HorizontalServerOnly.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -6,47 +7,89 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using System.Windows.Input;
 using System.Windows.Threading;
 using AFTP.Client.Enum;
 using AFTP.Client.Model.File;
 using AFTP.Client.Model.Util;
 using AFTP.Client.Model.View;
+using AFTP.Client.Window;
 using ListViewItem = System.Windows.Controls.ListViewItem;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace AFTP.Client.View.Explorer {
     /// <summary>
     /// Interaction logic for HorizontalServerOnly.xaml
     /// </summary>
     public partial class HorizontalServerOnly : FileExplorer {
-        public HorizontalServerOnly() {
-            InitializeComponent();
-        }
-
-        private string _currentRemotePath;
-        private GridViewColumnHeader _listViewSortCol = null;
-        private SortAdorner _listViewSortAdorner = null;
 
         public override event GoToRemotePathHandler OnGoToRemotePath;
         public override event GoToLocalPathHandler OnGoToLocalPath;
 
+        private string _currentRemotePath;
+        private GridViewColumnHeader _listViewSortCol = null;
+        private SortAdorner _listViewSortAdorner = null;
+        private readonly TreeViewEntry _defaultTreeViewItem;
+        private bool _allowSelectEvent = true;
+
+        public HorizontalServerOnly() {
+            InitializeComponent();
+            _defaultTreeViewItem = new TreeViewEntry("/", "");
+            RemoteTreeView.Items.Add(_defaultTreeViewItem);
+        }
+
         public override void UpdateRemotePath(string path) {
             _currentRemotePath = path;
             this.RemotePathBox.Dispatcher?.Invoke(DispatcherPriority.Normal, new Action(() => {
-                var lastChar = path[path.Length - 1];
-                if (lastChar != '/' && lastChar != '\\') path += "/";
                 RemotePathBox.Text = path;
             }));
+        }
+
+        public void LoadDirectories(string path, TreeViewEntry treeViewItem) {
+            if (path[0] == '/') path = path.Remove(0, 1);
+            var data = path.Split(new[] { '/' }, 2);
+            var subItem = treeViewItem.Children.Cast<TreeViewEntry>().FirstOrDefault(item => item.Directory.Equals(data[0]));
+            if (subItem == null) {
+                subItem = new TreeViewEntry(data[0], treeViewItem.Path + "/" + data[0]);
+                treeViewItem.Children.Add(subItem);
+            }
+            if (data.Length <= 1) return;
+            LoadDirectories(data[1], subItem);
+        }
+
+        public void SelectDirectory(string path, TreeViewEntry treeViewItem) {
+            if (path[0] == '/') path = path.Remove(0, 1);
+            var data = path.Split(new[] { '/' }, 2);
+            TreeViewEntry subItem = null;
+            foreach (var item in treeViewItem.Children) {
+                if (item.Directory.Equals(data[0])) subItem = item;
+            }
+            if (subItem == null) {
+                return;
+            }
+            if (data.Length <= 1) {
+                subItem.IsNodeSelected = true;
+                return;
+            }
+            SelectDirectory(data[1], subItem);
         }
 
         public override void UpdateLocalPath(string path) { }
 
         public override void UpdateRemoteEntries(RemoteEntry[] remoteEntries) {
-            RemoteListView.Items.Clear();
-            RemoteListView.Items.Add(new ListViewRemoteEntry());
-            foreach (var entry in remoteEntries) {
-                RemoteListView.Items.Add(new ListViewRemoteEntry(entry));
-            }
+            this.Dispatcher?.Invoke(DispatcherPriority.Normal, new Action(() => {
+                RemoteListView.Items.Clear();
+                RemoteListView.Items.Add(new ListViewRemoteEntry());
+                foreach (var entry in remoteEntries) {
+                    RemoteListView.Items.Add(new ListViewRemoteEntry(entry));
+                    if (entry.Type != RemoteEntryType.Directory) continue;
+                    LoadDirectories(entry.FullName, _defaultTreeViewItem);
+                }
+                _allowSelectEvent = false;
+                SelectDirectory(_currentRemotePath, _defaultTreeViewItem);
+                _allowSelectEvent = true;
+            }));
         }
 
         public override void UpdateLocalEntries(LocalEntry[] localEntries) { }
@@ -91,6 +134,13 @@ namespace AFTP.Client.View.Explorer {
             _listViewSortAdorner = new SortAdorner(_listViewSortCol, newDir);
             AdornerLayer.GetAdornerLayer(_listViewSortCol)?.Add(_listViewSortAdorner);
             RemoteListView.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+
+        private void RemoteTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            if (!_allowSelectEvent) return;
+            if (!(RemoteTreeView.SelectedItem is TreeViewEntry item)) return;
+            if (string.IsNullOrEmpty(item.Path)) OnGoToRemotePath?.Invoke(this, "/");
+            else OnGoToRemotePath?.Invoke(this, item.Path);
         }
     }
 }
